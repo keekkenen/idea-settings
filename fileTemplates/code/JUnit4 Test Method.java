a77f8e37a7be9176abb52d07b83be308faec3149
Eduard Kardashov <ekardashov@ifdsls.ru>
2019-06-11 11:06:42 +0300

    IU-2019.1.3 <kee@DESKTOP-JKE05GK Create markdown.xml, kee.xml, project.default.xml, diff.xml, Enum.java, Interface.java, Maven.xml, Default.xml, applicationLibraries.xml, JUnit4 Test Method.java, csv-plugin.xml, file.template.settings.xml, find.xml, checkstyle-idea.xml, keymap.xml, androidEditors.xml, code.style.schemes.xml, mavenVersion.xml, kee.icls, Default.xml, kee.xml, editor.codeinsight.xml, editor.xml, uml.xml, debugger.xml, updates.xml, baseRefactoring.xml, ide.general.xml, git.xml, exp.icls, eclipseCodeFormatter.xml, lombok-plugin.xml, postfixTemplates.xml, styleCode_old.xml, ui.lnf.xml, log_highlighting.xml, filetypes.xml, javaeeExternalResources.xml, remote-servers.xml, colors.scheme.xml, notifications.xml, databaseDrivers.xml, templates.xml, jprofiler.xml, laf.xml, Class.java, jdk.table.xml, BF.icls, path.macros.xml, intentionSettings.xml, Liquibase change log.xml, vcs.xml, databaseSettings.xml, toStringTemplates.xml, usageView.xml

---
+++ fileTemplates/code/JUnit4 Test Method.java
@@ -0,0 +1,6 @@
+@org.junit.Test
+public void ${NAME}() {
+  trace("${NAME}");
+  //FIXME kee: need implementation
+  org.junit.Assert.fail("method ${NAME} not yet implemented");
+}
---
+++ fileTemplates/internal/Enum.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) ${YEAR} iFD AG Chemnitz http://www.ifdag.de
+ */
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * {Description}
+ *
+ * @author ${USER}
+ */
+public enum ${NAME} {
+
+}
